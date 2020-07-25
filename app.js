ee9874b263b59e0cb3053fa1b1771b4cd2b80cbd
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-07-24 22:31:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const request=require('request');
 const url= "http://api.weatherstack.com/current?access_key=1cf99347ca0a03c7ed40e29ec1935609&query=+43.6532,-79.3832"
 //const url = "https://api.darksky.net/forecast/0af22b7c77bddf1e717288e5e34852fe/37.8267,-122.4233";
 
-request({url:url} ,(error, response)=>{
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url:url, json:true} ,(error, response)=>{
+    console.log("It is currently "+response.body.current.temperature+". It feels like "+response.body.current.feelslike+".")
+    
 });
 
