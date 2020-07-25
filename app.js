1978a85a7764afbd2bec2c858f4f4081e2ff74b1
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-07-25 18:14:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
 const request=require('request');
+const geocode=require('./utils/geocode')
+const forecast=require('./utils/forecast')
 //Toronto Weather
 const url= "http://api.weatherstack.com/current?access_key=1cf99347ca0a03c7ed40e29ec1935609&query=+43.6532,-79.3832"
-//const url="http://api.weatherstack.com/current?access_key=1cf99347ca0a03c7ed40e29ec1935609&query="
 
-request({url:url, json:true} ,(error, response)=>{
-    if(error){
-        console.log("No internet connection.")
-    }else if(response.body.error){
-        console.log("Location not found")
-    }else{
-        console.log("Location: "+response.body.location.name)    
-        console.log("Current condition: "+response.body.current.weather_descriptions[0])
-        console.log("It is currently "+response.body.current.temperature+". It feels like "+response.body.current.feelslike+".")
-    } 
-});
+geocode('Boston',(error, data)=>{
+    console.log('Error',error)
+    console.log('Data',data)
+    forecast(data.lat,data.long,(error,data)=>{
+        console.log('Error',error)
+        console.log('Data',data)
+    })
+})
 
-const geoURL="https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGFya2hvdGRvZyIsImEiOiJja2QyMnZqcDUwYmZ2MnhwNTM3Zzh4b2pzIn0.4M9yfbJ5oNFaR9bWjrBikQ&limit=1"
-//const geoURL="https://api.mapbox.com/geocoding/v5/mapbox.places/20Angeles.json?access_token=pk.eyJ1IjoiZGFya2hvdGRvZyIsImEiOiJja2QyMnZqcDUwYmZ2MnhwNTM3Zzh4b2pzIn0.4M9yfbJ5oNFaR9bWjrBikQ&limit=1"
-request({url:geoURL,json:true} ,(error, response)=>{
-    //console.log("Lat: "+response.body.features[0].center[1])
-    //console.log("Long: "+response.body.features[0].center[0])
-    if(error){
-        console.log("No internet connection.")
-    }else if(response.body.features.length==0){
-        console.log("Location not found")
-    }else{
-        const lat=response.body.features[0].center[1]
-        const long=response.body.features[0].center[0]
-        console.log(lat,long)
-    }   
-});
+
+
