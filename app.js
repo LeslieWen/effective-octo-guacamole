bfe8db19f443ebed1f8762c13ea054a039777e2e
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-07-25 18:49:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const request=require('request');
 const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast')
-//Toronto Weather
-const url= "http://api.weatherstack.com/current?access_key=1cf99347ca0a03c7ed40e29ec1935609&query=+43.6532,-79.3832"
 
-
-geocode(process.argv[2],(error, data)=>{
-    if (error){
-        return console.log(error)
-    }
-    forecast(data.lat,data.long,(error,data)=>{
-        if(error){
+if(process.argv[2]!=null){
+    geocode(process.argv[2],(error, data)=>{
+        if (error){
             return console.log(error)
         }
-        //console.log('Error',error)
-        console.log(data.location)
-        console.log(data.forecastData)
+        forecast(data.lat,data.long,(error,data)=>{
+            if(error){
+                return console.log(error)
+            }
+            //console.log('Error',error)
+            console.log(data.location)
+            console.log(data.forecastData)
+        })
     })
-})
-
+}else{
+    console.log("To query the weather of certain city, execute: node app.js \"CITY NAME\"")
+}
 
